3f90ee66af653386bfb05e5bc2aaa2370627c42b
Travis CI <travis@travis-ci.org>
2016-02-29 09:12:46 -0800

    refactor(travis): breaks out the deploy script

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,7 @@ set -e
 
 rm -rf deploy || exit 0;
 
-git clone --branch=master --single-branch "https://${GH_TOKEN}@${GH_REPOSITORY}" deploy # > /dev/null 2>&1
+git clone --branch=master --single-branch "https://${GH_TOKEN}@${GH_REPOSITORY}" deploy > /dev/null 2>&1
 cd deploy
 
 git rm -r .
@@ -14,5 +14,5 @@ cp ../public/.* .
 # git config user.email "travis@travis-ci.org"
 
 git add .
-git commit -am "`date +\"%D %T\"` [ci skip]"
-git push "https://${GH_TOKEN}@${GH_REPOSITORY}" master # > /dev/null 2>&1
+git commit -am "Travis buld #${$TRAVIS_BUILD_NUMBER} - `date +\"%D %T\"` [ci skip]"
+git push "https://${GH_TOKEN}@${GH_REPOSITORY}" master > /dev/null 2>&1
