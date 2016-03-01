ae115af5ea61e08276e6e40089bafb30008ce2e0
Travis CI <travis@travis-ci.org>
2016-02-29 21:30:49 -0800

    refactor(build): minify html, add some stuff to the page

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,16 @@
 import gulp from 'gulp';
+import ghtmlmin from 'gulp-htmlmin';
 
 gulp.task('default', () => {
     return gulp.src(['source/*.html'])
+        .pipe(ghtmlmin({
+            removeComments: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            removeTagWhitespace: true,
+            removeAttributeQuotes: true,
+            removeRedundantAttributes: true,
+            quoteCharacter: '"',
+        }))
         .pipe(gulp.dest('public'));
 })
