a7381d11cd3a38051723380708f6115a30397c14
Travis CI <travis@travis-ci.org>
2016-02-29 22:27:48 -0800

    feat(webpack config): adds webpack configs

---
+++ config/webpack/base.babel.js
@@ -0,0 +1,37 @@
+import path from 'path';
+
+import webpack from 'webpack';
+import ExtractText from 'extract-text-webpack-plugin';
+
+export default {
+    entry: {
+        main: './source/main.js',
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                plugins: ['transform-runtime'],
+                presets: ['es2015'],
+            },
+        }, {
+            test: /\.scss$/,
+            loader: ExtractText.extract('style', 'css!sass'),
+        }],
+    },
+
+    output: {
+        filename: '[name].js',
+        publicPath: '',
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+        }),
+        new ExtractText('main.css'),
+    ],
+};
---
+++ config/webpack/start.babel.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import { merge } from 'lodash';
+
+import base from './base.babel';
+
+export default merge(base, {
+    devServer: {
+        contentBase: './source',
+        stats: {
+            colors: true,
+        },
+    },
+
+    devtool: 'eval-source-map',
+});
