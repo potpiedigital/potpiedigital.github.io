06b5e31cc38d725cd09e6553879faf6e50888ab5
Mark Hayes <mark.m.hayes1@gmail.com>
2017-03-07 13:49:35 -0500

    just changing the HTML
    added divs to the body

--- source/projects/clock/main.js
+++ source/projects/clock/main.js
@@ -8,6 +8,10 @@ function assert(condition, message) {
 
 // assert (true===true, 'condition should be true');
 // assert (true===false, 'true should not equal false');
+assert(numberAsString(2)==='02', 'numberAsString(2) should return \'02\'');
+assert(numberAsString(10)==='10', 'numberAsString(10) should return \'10\'');
+
+
 
 const main = document.getElementById('main');
 const now = new Date();
@@ -22,8 +26,6 @@ function numberAsString(num) {
     }
     return num.toString();
 }
-assert(numberAsString(2)==='02', 'numberAsString(2) should return \'02\'');
-assert(numberAsString(10)==='10', 'numberAsString(10) should return \'10\'');
 
 setInterval(function () {
     seconds++;
