2b4a08db5b9554047cc5814c3d88bf76aff77d78
Mark Hayes <mark.m.hayes1@gmail.com>
2017-03-07 20:26:05 -0500

    Not quite right
    still not right, but I think its on the right track.
    
    doesn’t seem to want to update in the individual divs by ID, still only
    in the main.innerText.
    Also only hours are displaying right now.
    Also, I don’t think the code is correct as I have 3 set interval
    functions.

--- source/projects/clock/main.js
+++ source/projects/clock/main.js
@@ -13,12 +13,41 @@ assert(numberAsString(10)==='10', 'numberAsString(10) should return \'10\'');
 
 
 
-const main = document.getElementById('main');
-const now = new Date();
+// const main = document.getElementById('main');
+// const now = new Date();
 
+// let seconds = now.getSeconds();
+// let minutes = now.getMinutes();
+// let hours = now.getHours();
+
+// function numberAsString(num) {
+//     if (num < 10) {
+//         return '0' + num.toString();
+//     }
+//     return num.toString();
+// }
+
+// setInterval(function () {
+//     seconds++;
+//     if (seconds >=60){
+//         minutes++;
+//         seconds=0;
+//     }
+//     if (minutes>=60){
+//         hours++;
+//         minutes=0;
+//     }
+//     if (hours>=13){
+//         hours = hours - 12;
+//     }
+//     main.innerText = numberAsString(hours) + ':' + numberAsString(minutes) + ':' + numberAsString(seconds);
+// }, 1000);
+
+
+// seconds
+const mainSeconds = document.getElementById('seconds');
+const now = new Date();
 let seconds = now.getSeconds();
-let minutes = now.getMinutes();
-let hours = now.getHours();
 
 function numberAsString(num) {
     if (num < 10) {
@@ -33,14 +62,58 @@ setInterval(function () {
         minutes++;
         seconds=0;
     }
+    // if (minutes>=60){
+    //     hours++;
+    //     minutes=0;
+    // }
+    // if (hours>=13){
+    //     hours = hours - 12;
+    // }
+    main.innerText = ':' + numberAsString(seconds);
+},1000);
+
+// minutes
+
+const mainMinutes = document.getElementById('minutes');
+let minutes = now.getMinutes();
+
+function numberAsString(num) {
+    if (num < 10) {
+        return '0' + num.toString();
+    }
+    return num.toString();
+}
+
+setInterval(function () {
+    // seconds++;
+    // if (seconds >=60){
+    //     minutes++;
+    //     seconds=0;
+    // }
     if (minutes>=60){
         hours++;
         minutes=0;
     }
+    minutes.innerText = numberAsString(minutes) + ':';
+},1000);
+
+// Hours
+const mainHours = document.getElementById('hours');
+let hours = now.getHours();
+
+setInterval(function () {
+    // seconds++;
+    // if (seconds >=60){
+    //     minutes++;
+    //     seconds=0;
+    // }
+    // if (minutes>=60){
+    //     hours++;
+    //     minutes=0;
+    // }
     if (hours>=13){
         hours = hours - 12;
     }
-    main.innerText = numberAsString(hours) + ':' + numberAsString(minutes) + ':' + numberAsString(seconds);
-}, 1000);
-
+    main.innerText = numberAsString(hours) + ':';
+},1000);
 
