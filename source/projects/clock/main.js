587fce190f10794cc04c9b0322a57d262eda7e78
Mark Hayes <mark.m.hayes1@gmail.com>
2017-03-14 21:39:13 -0400

    updates so it works

--- source/projects/clock/main.js
+++ source/projects/clock/main.js
@@ -13,40 +13,14 @@ assert(numberAsString(10)==='10', 'numberAsString(10) should return \'10\'');
 
 
 
-// const main = document.getElementById('main');
-// const now = new Date();
-
-// let seconds = now.getSeconds();
-// let minutes = now.getMinutes();
-// let hours = now.getHours();
-
-// function numberAsString(num) {
-//     if (num < 10) {
-//         return '0' + num.toString();
-//     }
-//     return num.toString();
-// }
-
-// setInterval(function () {
-//     seconds++;
-//     if (seconds >=60){
-//         minutes++;
-//         seconds=0;
-//     }
-//     if (minutes>=60){
-//         hours++;
-//         minutes=0;
-//     }
-//     if (hours>=13){
-//         hours = hours - 12;
-//     }
-//     main.innerText = numberAsString(hours) + ':' + numberAsString(minutes) + ':' + numberAsString(seconds);
-// }, 1000);
-
-
-// seconds
 const mainSeconds = document.getElementById('seconds');
+const mainMinutes = document.getElementById('minutes');
+const mainHours = document.getElementById('hours');
+
 const now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
 let seconds = now.getSeconds();
 
 function numberAsString(num) {
@@ -55,58 +29,34 @@ function numberAsString(num) {
     }
     return num.toString();
 }
+function updateElement(element,text) {
+    element.innerText=text;
+}
+if (hours>=13){
+    hours = hours - 12;
+}
+updateElement(mainSeconds, numberAsString(seconds));
+updateElement(mainMinutes, numberAsString(minutes));
+updateElement(mainHours, numberAsString(hours));
 
 setInterval(function () {
     seconds++;
     if (seconds >=60){
         minutes++;
         seconds=0;
+        if (minutes>=60){
+            hours++;
+            minutes=0;
+            if (hours>=13){
+                hours = hours - 12;
+            }
+            updateElement(mainHours,numberAsString(hours));
+        }
+        updateElement(mainMinutes,numberAsString(minutes));
     }
-    // if (minutes>=60){
-    //     hours++;
-    //     minutes=0;
-    // }
-    // if (hours>=13){
-    //     hours = hours - 12;
-    // }
-    main.innerText = ':' + numberAsString(seconds);
+    updateElement(mainSeconds,numberAsString(seconds));
 },1000);
 
-// minutes
-const mainMinutes = document.getElementById('minutes');
-let minutes = now.getMinutes();
-
-setInterval(function () {
-    // seconds++;
-    // if (seconds >=60){
-    //     minutes++;
-    //     seconds=0;
-    // }
-    if (minutes>=60){
-        hours++;
-        minutes=0;
-    }
-    main.innerText = numberAsString(minutes) + ':';
-},60000);
-
-// Hours
-const mainHours = document.getElementById('hours');
-let hours = now.getHours();
-
-setInterval(function () {
-    // seconds++;
-    // if (seconds >=60){
-    //     minutes++;
-    //     seconds=0;
-    // }
-    // if (minutes>=60){
-    //     hours++;
-    //     minutes=0;
-    // }
-    if (hours>=13){
-        hours = hours - 12;
-    }
-    main.innerText = numberAsString(hours);
-    // main.innerText = numberAsString(hours) + ':' + numberAsString(minutes) + ':' + numberAsString(seconds);
-},3.6e+6);
+// new fucntion update (hours, minutes, seconds just as numbers )
+// updates the correct elements at the correct time.
 
